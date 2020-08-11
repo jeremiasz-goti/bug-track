b1fc6c92476ebd03a4d275ee1d74d47e7426cb05
jeremiasz-goti <jeremiasz.goti@gmail.com>
2020-08-11 21:17:18 +0200

    auth and user accounts work

--- bugtrack/routings.py
+++ bugtrack/routings.py
@@ -1,15 +1,18 @@
-from flask import render_template, url_for, flash, redirect
+from flask import render_template, url_for, flash, redirect, request
 from bugtrack.forms import RegistrationForm, LoginForm, IssueForm
 from bugtrack.models import User, Issue
 from bugtrack import app, db, bcrypt
+from flask_login import login_user, logout_user, current_user, login_required
 
 
 
 @app.route('/')
+@app.route('/home')
 def home():
     return render_template('home.html')
 
 @app.route('/add', methods=['GET', 'POST'])
+@login_required
 def add():
     form = IssueForm()
     if form.validate_on_submit():
@@ -17,22 +20,14 @@ def add():
     return render_template('add.html', title='Add Issue', form=form)
 
 @app.route('/reports')
+@login_required
 def reports():
     return render_template('reports.html', title='My reports')
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User(email=form.email.data, password=form.password.data)
-        flash(f'User logged in.', 'success')
-        return redirect(url_for('home'))
-    else:
-        flash(f'Error - check username and password', 'danger')
-    return render_template('login.html', title='Login', form=form)
-
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
@@ -42,3 +37,30 @@ def register():
         flash(f'Account created.', 'success')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember_user.data)
+            send_to = request.args.get('next')
+            return redirect(send_to) if send_to else redirect(url_for('home'))
+        else:
+            flash(f'Error - check email and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+@app.route('/settings')
+@login_required
+def settings():
+    return render_template('settings.html', title='Settings')
+
+
